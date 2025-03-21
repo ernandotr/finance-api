6af1906dc99f184b71fb91a7d8cc35ed54a10a4f
Ernando Rezende <ernando.03@gmail.com>
2025-03-21 23:42:57 +0000

    Crete unit teste for the getById method

--- src/test/java/dev/ernandorezende/financeapi/doman/services/TransactionServiceTest.java
+++ src/test/java/dev/ernandorezende/financeapi/doman/services/TransactionServiceTest.java
@@ -1,14 +1,24 @@
 package dev.ernandorezende.financeapi.doman.services;
 
+import dev.ernandorezende.financeapi.application.responses.CategoryResponse;
+import dev.ernandorezende.financeapi.application.responses.TransactionResponse;
+import dev.ernandorezende.financeapi.domain.models.Category;
+import dev.ernandorezende.financeapi.domain.models.Transaction;
 import dev.ernandorezende.financeapi.domain.services.TransactionService;
 import dev.ernandorezende.financeapi.infra.reposirories.TransactionRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.verify;
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TransactionServiceTest {
@@ -24,5 +34,19 @@ public class TransactionServiceTest {
         transactionService.getAll();
 
         verify(transactionRepository).findAll();
+    }
+
+    @Test
+    void getTransactiponByIdSuccess() {
+        CategoryResponse categoryResponse = new CategoryResponse(1, "Market");
+        var expectedTransaction = new TransactionResponse(1L, "Cobtinente", "Wallet", BigDecimal.TEN, categoryResponse);
+        var category = new Category(1, "Market");
+        var transaction =  new Transaction("Continente", BigDecimal.TEN, "Wallet", category);
+        when(transactionRepository.findById(anyLong())).thenReturn(Optional.of(transaction));
+        transactionService.getById(1L);
+
+        verify(transactionRepository, times(1)).findById(anyLong());
+
+        Assertions.assertThat(expectedTransaction.id()).isEqualTo(1L);
     }
 }
